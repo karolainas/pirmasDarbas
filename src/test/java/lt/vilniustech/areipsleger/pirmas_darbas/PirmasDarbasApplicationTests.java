8bb3bcd53e6284a73b3b09aab8a7732048b7ac00
Karolis <karolismrutkauskas@gmail.com>
2025-02-25 16:02:45 +0200

    ai ten

---
+++ src/test/java/lt/vilniustech/areipsleger/pirmas_darbas/PirmasDarbasApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vilniustech.areipsleger.pirmas_darbas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PirmasDarbasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
